97c6165f21f4a6cac59f794653dc656dd06b5b11
Daniil Konovalenko <Zest005@gmail.com>
2026-04-16 14:00:58 +0300

    Added Domain entities

---
+++ CommentsApp.Domain/Entities/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommentsApp.Domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        // валидация
+        public string Email { get; set; } = string.Empty;
+        // валидация URL
+        public string? HomePage { get; set; }
+        // защита от XSS
+        public string Text { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        // null - родительский коммент, !null - ответ
+        public int? ParentCommentId { get; set; }
+
+        public Comment? ParentComment { get; set; }
+        public ICollection<Comment> Replies { get; set; } = new List<Comment>();
+        public CommentAttachment? Attachment { get; set; }
+    }
+}
---
+++ CommentsApp.Domain/Entities/CommentAttachment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommentsApp.Domain.Entities
+{
+    public class CommentAttachment
+    {
+        public int Id { get; set; }
+        public int CommentId { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string StoredFilePath { get; set; } = string.Empty;
+        public string ContentType { get; set; } = string.Empty;
+        // ограничения по размеру для .txt
+        public long FileSize { get; set; }
+
+        public Comment Comment { get; set; } = null!;
+    }
+}
