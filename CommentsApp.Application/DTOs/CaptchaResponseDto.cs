ac63960d58655d81fe34e0a0b58883b27d2a2bf6
Daniil Konovalenko <Zest005@gmail.com>
2026-04-19 15:44:23 +0300

    Apply static reply model to CaptchaController

---
+++ CommentsApp.Application/DTOs/CaptchaResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CommentsApp.Application.DTOs
+{
+    public class CaptchaResponseDto
+    {
+        public string CaptchaId { get; set; } = string.Empty;
+        public string ImageBase64 { get; set; } = string.Empty;
+    }
+}
