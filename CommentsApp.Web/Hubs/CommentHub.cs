b462e61779e5a8325be47e34b501ae462f19f5a4
Daniil Konovalenko <Zest005@gmail.com>
2026-04-19 15:50:59 +0300

    Remove empty override's

--- CommentsApp.Web/Hubs/CommentHub.cs
+++ CommentsApp.Web/Hubs/CommentHub.cs
@@ -2,16 +2,5 @@
 
 namespace CommentsApp.Web.Hubs
 {
-    public class CommentHub : Hub
-    {
-        public override async Task OnConnectedAsync()
-        {
-            await base.OnConnectedAsync();
-        }
-
-        public override async Task OnDisconnectedAsync(Exception? exception)
-        {
-            await base.OnDisconnectedAsync(exception);
-        }
-    }
+    public class CommentHub : Hub { }
 }
